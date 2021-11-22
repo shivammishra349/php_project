aae52af879cea08b98ddad1b4829c6bdaeb431f6
shivam mishra <shivam9.mishra1999@gmail.com>
2021-11-22 19:24:22 +0530

    file added

--- dashboard.php
+++ dashboard.php
@@ -3,11 +3,11 @@
 require("header.php");
 
 ?>
-<
-<!--<a href="add_form.php"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="45" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+
+<a href="add_form.php"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="45" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
   <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
   <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-</svg></a> >-->
+</svg></a> 
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -19,7 +19,10 @@ require("header.php");
 				<th>NAME</th>
 				<th>AGE</th>
 				<th>CITY</th>
-				<!--<th>ACTION</th>-->
+				<th>added_date</th>
+				<th>updated_date</th>
+				<th>status</th>
+				<th>ACTION</th>
 			</thead>
 			<tbody>
 			
@@ -31,15 +34,20 @@ require("header.php");
 						<td><?php echo $row['name']; ?></td>
 						<td><?php echo $row['age']; ?></td>
 						<td><?php echo $row['city']; ?></td>
+						<td><?php echo $row['added_date']; ?></td>
+						<td><?php echo $row['updated_date']; ?></td>
+						<td><?php echo $row['status']; ?></td>
+
+
 						<td>
-							<!--<a href="#"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-vector-pen" viewBox="0 0 16 16">
+							<a href="#"><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-vector-pen" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828L10.646.646zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z"/>
   <path fill-rule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086.086-.026z"/>
 </svg></button></a>
 							&nbsp; &nbsp;
 							<button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
   <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-</svg></button>-->
+</svg></button>
 						</td>
 
 					</tr>
--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 
 require("function.php");
 
-$conn= getConn('localhost', 'diituser', '%TGBbgt5','php');
+$conn= getConn('localhost', 'diituser', '%TGBbgt5','diit');
 
-$stmt= $conn->prepare("SELECT * FROM users");
+$stmt= $conn->prepare("SELECT * FROM shivam_user");
 $stmt->execute();
 $result= $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $records = $stmt->fetchAll();
